a0f19f60067d708f3390b7650015059e8c56ecf8
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-15 20:58:04 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AnalyserProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program !!!");
+        }
+    }
+}
